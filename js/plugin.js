86d51dcadac7aa83821e96aecd82e895d31209c6
abdulazizasiri <aasiri@lion.lmu.edu>
2018-08-13 02:03:34 -0700

    added the layout of windows 8

--- js/plugin.js
+++ js/plugin.js
@@ -4,13 +4,13 @@
 //
 //
 
-
-$("body").on("click",function(){
-  let x = event.pageX ;
-  let y = event.pageY;
-  .addClass("circle").css({
-    left: x,
-    right: y
-  })
-
-})
+//
+// $("body").on("click",function(){
+//   let x = event.pageX ;
+//   let y = event.pageY;
+//   .addClass("circle").css({
+//     left: x,
+//     right: y
+//   })
+//
+// })
